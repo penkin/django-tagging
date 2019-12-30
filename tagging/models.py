ed79a29e08868f6986494d93ce5e6acd605eb7a1
Christopher Penkin <chris@digitalsolutionfoundry.co.za>
2019-12-30 12:27:44 +0200

    moved from using Django extentions to six for python_2_unicode_compatible.

--- tagging/models.py
+++ tagging/models.py
@@ -4,10 +4,10 @@ Models and managers for tagging.
 from django.db import models
 from django.db import connection
 from django.utils.encoding import smart_text
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from six import python_2_unicode_compatible
 
 from tagging import settings
 from tagging.utils import LOGARITHMIC
